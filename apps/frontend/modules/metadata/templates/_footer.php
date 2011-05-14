1c6edb9b99ad07441a05125927a4d2726eb6f06a
modul <modul@80bdb1b5-c28a-45bd-a507-38828ad3acfd>
2011-05-14 15:38:37 +0000

    podpięty facebook z właściwym profilem
    git-svn-id: https://xp-dev.com/svn/SmashinSubversion@161 80bdb1b5-c28a-45bd-a507-38828ad3acfd

--- apps/frontend/modules/metadata/templates/_footer.php
+++ apps/frontend/modules/metadata/templates/_footer.php
@@ -16,7 +16,7 @@
                     <?endif;?>
                     <?if($facebook):?>
                         <div id="bf-facebook">
-                            <iframe src="http://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Fplatform&amp;width=531&amp;colorscheme=light&amp;show_faces=true&amp;stream=false&amp;header=false&amp;height=254" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:531px; height:254px;" allowTransparency="true"></iframe>
+                            <iframe src="http://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Fpages%2FSmashin-Tracks%2F214164275274674&amp;width=531&amp;colorscheme=light&amp;show_faces=true&amp;stream=false&amp;header=false&amp;height=254" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:531px; height:254px;" allowTransparency="true"></iframe>
                         </div>
                     <?endif;?>
                     <div class="clear"></div>
